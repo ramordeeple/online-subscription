2f590d56b05bc5e2d92e9dd45e69d0ce7e9f26c7
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-27 19:14:06 +0300

    req and resp

---
+++ internal/handler/http/request.go
@@ -0,0 +1,9 @@
+package http
+
+type CreateSubscriptionRequest struct {
+	ServiceName string `json:"service_name"`
+	Price       int    `json:"price"`
+	UserID      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+	EndDate     string `json:"end_date"`
+}
---
+++ internal/handler/http/response.go
@@ -0,0 +1,10 @@
+package http
+
+type SubscriptionResponse struct {
+	ID          string `json:"id"`
+	ServiceName string `json:"service_name"`
+	Price       int    `json:"price"`
+	UserID      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+	EndDate     string `json:"end_date"`
+}
