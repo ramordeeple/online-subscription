e42e98bc4224a07ac9bb1c1ba2a074e07127984e
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-27 18:18:23 +0300

    adding repository.go

---
+++ internal/repository/repository.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+	"online-subscription/internal/model"
+)
+
+type SubscriptionFilter struct {
+	UserID  *string
+	Service *string
+}
+
+type SummaryFilter struct {
+	UserID      *string
+	ServiceName *string
+	FromMonth   int
+	FromYear    int
+	ToMonth     int
+	toYear      int
+}
+
+type SubscriptionRepository interface {
+	Create(ctx context.Context, s *model.Subscription) error
+	Get(ctx context.Context, id string) (*model.Subscription, error)
+	Update(ctx context.Context, s *model.Subscription) error
+	Delete(ctx context.Context, id string) error
+	List(ctx context.Context, filter *SubscriptionFilter) ([]*model.Subscription, error)
+	Sum(ctx context.Context, filter *SummaryFilter) (int, error)
+}
