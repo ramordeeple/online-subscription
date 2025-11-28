63f4cbced65788e29827293da5beca47cf05c729
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-28 14:32:19 +0300

    routers

---
+++ internal/app/router.go
@@ -0,0 +1,49 @@
+package app
+
+import (
+	"net/http"
+	"online-subscription/internal/handler"
+	"strings"
+)
+
+func NewRouter(h *handler.SubscriptionHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("subscriptions", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			h.List(w, r)
+		case http.MethodPost:
+			h.Create(w, r)
+		default:
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	mux.HandleFunc("/subscriptions", func(w http.ResponseWriter, r *http.Request) {
+		id := strings.TrimPrefix(r.URL.Path, "subscriptions/")
+		if id == "" {
+			http.Error(w, "id is required", http.StatusBadRequest)
+			return
+		}
+
+		switch r.Method {
+		case http.MethodGet:
+			h.GetById(w, r, id)
+		case http.MethodDelete:
+			h.Delete(w, r, id)
+		default:
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	mux.HandleFunc("subscriptions/summary", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		h.Summary(w, r)
+	})
+
+	return mux
+}
