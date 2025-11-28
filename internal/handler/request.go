b8f229ff3b4744b11a1701b1eee468d0ffc40258
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-28 22:59:41 +0300

    deleting obsolete

--- internal/handler/request.go
+++
@@ -1,9 +0,0 @@
-package handler
-
-type CreateSubscriptionRequest struct {
-	ServiceName string `json:"service_name"`
-	Price       int    `json:"price"`
-	UserID      string `json:"user_id"`
-	StartDate   string `json:"start_date"`
-	EndDate     string `json:"end_date"`
-}
