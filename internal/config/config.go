6b02d546373589589c04d92e6f8b6f959af1d3a1
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-28 16:31:33 +0300

    adding conf

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,15 +1,46 @@
 package config
 
-import "os"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
 
 type Config struct {
-	Port string
-	DSN  string
+	App struct {
+		Port string `yaml:"port"`
+	} `yaml:"app"`
+	DB struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+		SSLMode  string `yaml:"sslmode"`
+	} `yaml:"db"`
 }
 
-func LoadConfig() *Config {
-	return &Config{
-		Port: os.Getenv("PORT"),
-		DSN:  os.Getenv("POSTGRES_DSN"),
+func (c *Config) DSN() string {
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		c.DB.Host, c.DB.Port, c.DB.User, c.DB.Password, c.DB.Name, c.DB.SSLMode,
+	)
+}
+
+func LoadConfig(path string) *Config {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("failed to open config file: %v", err)
 	}
+	defer file.Close()
+
+	var cfg Config
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&cfg); err != nil {
+		log.Fatalf("failed to parse config.yaml: %v", err)
+	}
+
+	return &cfg
 }
