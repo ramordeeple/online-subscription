69418359e8419931c8f41f3b9448a53347799f8c
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-28 16:34:50 +0300

    adding .sql

---
+++ migrations/init_001.sql
@@ -0,0 +1,10 @@
+CREATE TABLE subscriptions (
+    id UUID PRIMARY KEY,
+    service_name TEXT NOT NULL,
+    price INT NOT NULL,
+    user_id UUID NOT NULL,
+    start_month INT NOT NULL,
+    start_year INT NOT NULL,
+    end_month INT,
+    end_year INT
+);
