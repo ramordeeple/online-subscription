19c13454b42926b8b25f432cf2061d56119b3271
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-28 15:41:16 +0300

    refactored subscription repo

--- internal/repository/postgres/subscription_repo.go
+++ internal/repository/postgres/subscription_repo.go
@@ -6,7 +6,8 @@ import (
 	"errors"
 	"fmt"
 	"online-subscription/internal/model"
-	"online-subscription/internal/repository"
+
+	_ "github.com/lib/pq"
 )
 
 type SubscriptionRepo struct {
@@ -19,21 +20,34 @@ func NewSubscriptionRepo(db *sql.DB) *SubscriptionRepo {
 
 func (r *SubscriptionRepo) Create(ctx context.Context, s *model.Subscription) error {
 	query := `
-		INSERT INTO subscriptions (id, service_name, price, user_id, start_month, start_year, end_month, end_year)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-		`
-	_, err := r.db.ExecContext(ctx, query, s.ID, s.ServiceName,
-		s.Price, s.UserID, s.StartYear, s.EndMonth, s.EndYear)
+		INSERT INTO subscriptions (
+			id, service_name, price, user_id, start_month, start_year, end_month, end_year
+		) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)
+	`
+
+	_, err := r.db.ExecContext(ctx, query,
+		s.ID, s.ServiceName, s.Price, s.UserID,
+		s.StartMonth, s.StartYear, s.EndMonth, s.EndYear,
+	)
 
 	return err
 }
 
 func (r *SubscriptionRepo) Get(ctx context.Context, id string) (*model.Subscription, error) {
-	query := `SELECT id, service_name, price, user_id, start_month, start_year, end_month, end_year FROM subscriptions WHERE id=$1`
+	query := `
+		SELECT id, service_name, price, user_id, start_month, start_year, end_month, end_year 
+		FROM subscriptions 
+		WHERE id=$1
+	`
 
 	row := r.db.QueryRowContext(ctx, query, id)
+
 	var s model.Subscription
-	err := row.Scan(&s.ID, &s.ServiceName, &s.Price, &s.UserID, s.StartMonth, s.StartYear, &s.EndMonth, &s.EndYear)
+	err := row.Scan(
+		&s.ID, &s.ServiceName, &s.Price, &s.UserID,
+		&s.StartMonth, &s.StartYear, &s.EndMonth, &s.EndYear,
+	)
+
 	if errors.Is(err, sql.ErrNoRows) {
 		return nil, nil
 	}
@@ -45,11 +59,14 @@ func (r *SubscriptionRepo) Update(ctx context.Context, s *model.Subscription) er
 	query := `
 		UPDATE subscriptions
 		SET service_name=$1, price=$2, user_id=$3, start_month=$4, start_year=$5, end_month=$6, end_year=$7
-		WHERE id=$8`
+		WHERE id=$8
+	`
 
 	_, err := r.db.ExecContext(ctx, query,
 		s.ServiceName, s.Price, s.UserID,
-		s.StartMonth, s.StartYear, s.EndMonth, s.EndYear, s.ID)
+		s.StartMonth, s.StartYear, s.EndMonth, s.EndYear,
+		s.ID,
+	)
 
 	return err
 }
@@ -59,19 +76,25 @@ func (r *SubscriptionRepo) Delete(ctx context.Context, id string) error {
 	return err
 }
 
-func (r *SubscriptionRepo) List(ctx context.Context, f repository.SubscriptionFilter) ([]*model.Subscription, error) {
-	query := `SELECT id, service_name, price, user_id, start_month, start_year, end_month, end_year FROM subscriptions WHERE 1=1`
+func (r *SubscriptionRepo) List(ctx context.Context, f *model.SubscriptionFilter) ([]*model.Subscription, error) {
+	query := `
+		SELECT id, service_name, price, user_id, start_month, start_year, end_month, end_year
+		FROM subscriptions
+		WHERE 1=1
+	`
+
 	args := []any{}
 	i := 1
 
 	if f.UserID != nil {
 		query += ` AND user_id = $` + fmt.Sprint(i)
-		args = append(args, *f.UserID)
+		args = append(args, f.UserID)
 		i++
 	}
+
 	if f.ServiceName != nil {
 		query += ` AND service_name = $` + fmt.Sprint(i)
-		args = append(args, *f.ServiceName)
+		args = append(args, f.ServiceName)
 		i++
 	}
 
@@ -81,10 +104,11 @@ func (r *SubscriptionRepo) List(ctx context.Context, f repository.SubscriptionFi
 	}
 	defer rows.Close()
 
-	subs := []*model.Subscription{}
+	var subs []*model.Subscription
 
 	for rows.Next() {
 		var s model.Subscription
+
 		err := rows.Scan(
 			&s.ID, &s.ServiceName, &s.Price, &s.UserID,
 			&s.StartMonth, &s.StartYear, &s.EndMonth, &s.EndYear,
@@ -99,21 +123,24 @@ func (r *SubscriptionRepo) List(ctx context.Context, f repository.SubscriptionFi
 	return subs, nil
 }
 
-func (r *SubscriptionRepo) Sum(ctx context.Context, f repository.SummaryFilter) (int, error) {
+func (r *SubscriptionRepo) Sum(ctx context.Context, f *model.SummaryFilter) (int, error) {
 	query := `
-		SELECT SUM(price) FROM subscriptions
-		WHERE (start_year*12 + start_month) <= ($1*12+$2)
-`
-	args := []any{f.ToMonth, f.FromYear, f.FromMonth}
-	i := 5
+		SELECT SUM(price) 
+		FROM subscriptions
+		WHERE (start_year*12 + start_month) <= ($1*12 + $2)
+	`
+
+	args := []any{f.ToYear, f.ToMonth}
+	i := 3
 
 	if f.UserID != nil {
-		query += ` AND user_id = $` + sqlIndex(i)
-		args = append(args, *f.UserID)
+		query += ` AND user_id = $` + fmt.Sprint(i)
+		args = append(args, f.UserID)
 		i++
 	}
 
 	var sum sql.NullInt64
+
 	err := r.db.QueryRowContext(ctx, query, args...).Scan(&sum)
 	if err != nil {
 		return 0, err
@@ -124,8 +151,4 @@ func (r *SubscriptionRepo) Sum(ctx context.Context, f repository.SummaryFilter) 
 	}
 
 	return int(sum.Int64), nil
-}
-
-func sqlIndex(i int) string {
-	return fmt.Sprintf("%d", i)
 }
