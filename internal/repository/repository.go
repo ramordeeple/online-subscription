584d3a71157510f183d190a4250afac732c5dfee
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-28 16:19:30 +0300

    refactored repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,25 +5,11 @@ import (
 	"online-subscription/internal/model"
 )
 
-type SubscriptionFilter struct {
-	UserID      *string
-	ServiceName *string
-}
-
-type SummaryFilter struct {
-	UserID      *string
-	ServiceName *string
-	FromMonth   int
-	FromYear    int
-	ToMonth     int
-	ToYear      int
-}
-
 type SubscriptionRepository interface {
 	Create(ctx context.Context, s *model.Subscription) error
 	Get(ctx context.Context, id string) (*model.Subscription, error)
 	Update(ctx context.Context, s *model.Subscription) error
 	Delete(ctx context.Context, id string) error
-	List(ctx context.Context, filter *SubscriptionFilter) ([]*model.Subscription, error)
-	Sum(ctx context.Context, filter *SummaryFilter) (int, error)
+	List(ctx context.Context, filter *model.SubscriptionFilter) ([]*model.Subscription, error)
+	Sum(ctx context.Context, filter *model.SummaryFilter) (int, error)
 }
