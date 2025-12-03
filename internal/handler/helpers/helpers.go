19a61287892784af84e0a20cbbc74936d868556d
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-12-03 22:30:08 +0300

    refactored helpers.go

--- internal/handler/helpers/helpers.go
+++ internal/handler/helpers/helpers.go
@@ -8,33 +8,19 @@ import (
 	"time"
 )
 
-func ParseDate(str string) (int, int, error) {
-	str = strings.TrimSpace(str)
-	t, err := time.Parse("01-2006", str)
-	if err != nil {
-
-		return 0, 0, fmt.Errorf("invalid date format, expected MM-YYYY")
-	}
-
-	return int(t.Month()), t.Year(), nil
-}
-
-func PtrString(s string) *string {
-	if s == "" {
-		return nil
-	}
-	return &s
-}
-
-func SafeString(s *string) string {
-	if s == nil {
-		return ""
-	}
-	return *s
-}
-
 func WriteJSON(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(data)
+}
+
+func ParseDateToTime(str string) (time.Time, error) {
+	str = strings.TrimSpace(str)
+
+	t, err := time.Parse("01-2006", str)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("invalid date format, expected MM-YYYY")
+	}
+
+	return t, nil
 }
