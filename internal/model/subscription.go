729b3a6fc971d59d108b4bcf12aa13d67b0767c2
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-12-03 07:39:18 +0300

    refactoring model

--- internal/model/subscription.go
+++ internal/model/subscription.go
@@ -1,26 +1,28 @@
 package model
 
+import "time"
+
+type Subscription struct {
+	ID          string     `json:"id"`
+	ServiceName string     `json:"service_name"`
+	Price       int        `json:"monthly_price"`
+	UserID      string     `json:"user_id"`
+	StartDate   time.Time  `json:"start_date"`
+	EndDate     *time.Time `json:"end_date"`
+}
+
 type SubscriptionFilter struct {
 	UserID      *string
 	ServiceName *string
+	FromDate    *time.Time
+	ToDate      *time.Time
+	Limit       *int
+	Offset      *int
 }
 
 type SummaryFilter struct {
 	UserID      *string
 	ServiceName *string
-	FromMonth   int
-	FromYear    int
-	ToMonth     *int
-	ToYear      *int
-}
-
-type Subscription struct {
-	ID          string
-	ServiceName string
-	Price       int
-	UserID      string
-	StartMonth  int
-	StartYear   int
-	EndMonth    *int
-	EndYear     *int
+	FromDate    time.Time
+	ToDate      time.Time
 }
