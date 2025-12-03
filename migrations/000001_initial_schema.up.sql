0f51731d0f7d663da76d598bee42e9a556ada9c4
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-12-03 07:31:00 +0300

    refactoring migrations

--- migrations/000001_initial_schema.up.sql
+++ migrations/000001_initial_schema.up.sql
@@ -1,11 +1,24 @@
 CREATE TABLE subscriptions
 (
-    id           UUID PRIMARY KEY,
-    service_name TEXT NOT NULL,
-    price        INT  NOT NULL,
-    user_id      UUID NOT NULL,
-    start_month  INT  NOT NULL,
-    start_year   INT  NOT NULL,
-    end_month    INT,
-    end_year     INT
+    id            UUID PRIMARY KEY,
+    service_name  TEXT        NOT NULL,
+    monthly_price INT         NOT NULL,
+    user_id       UUID        NOT NULL,
+    start_date    DATE        NOT NULL,
+    end_date      DATE,
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    CHECK (monthly_price > 0)
 );
+
+CREATE INDEX idx_subscriptions_user_id
+    ON subscriptions (user_id);
+
+CREATE INDEX idx_subscriptions_service_name
+    ON subscriptions (service_name);
+
+CREATE INDEX idx_subscriptions_start_date
+    ON subscriptions (start_date);
+
+CREATE INDEX idx_subscriptions_end_date
+    ON subscriptions (end_date);
